b14d464163ff2226250995d25b23f52d5fb301cc
mrogach2350 <m.rogach@gmail.com>
2016-09-14 12:15:58 -0700

    adds controller to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,32 @@
  * This is your main angular file. Edit as you see fit.
  *
  */
+angular
+  .module('tunely', [])
+  .controller('AlbumsIndexController', AlbumsIndexController);
+
+function AlbumsIndexController () {
+  var vm = this;
+  vm.newAlbum = {};
+
+  vm.newAlbum = {
+    name: 'How Big, How Blue, How Beautiful',
+    artistName: 'Florence + The Machine'
+  };
+
+  vm.albums = [
+    {
+      name: 'Coming Home',
+      artistName: 'Leon Bridges'
+    },
+    {
+      name: 'Are We There',
+      artistName: 'Sharon Van Etten'
+    },
+    {
+      name: 'The Queen is Dead',
+      artistName: 'The Smiths'
+    }
+  ];
+
+}
